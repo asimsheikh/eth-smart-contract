90cf916e289ef85f00da510b36d7afaca166b275
Asim Sheikh <asim.sardar.sheikh@gmail.com>
2022-06-12 10:15:31 +0200

    Update NomadicAsim.sol

--- NomadicAsim.sol
+++ NomadicAsim.sol
@@ -1,8 +1,10 @@
+// Contract on Ethereum MainNet 
+// Contract Address 0x30450f4bdd059c52d35ecf6cc7dbd5407e4322a6
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract NomadicAsim {
-	string message1 = "The pain will be worth it.";
+contract NomadicAsim { 
+    string message1 = "The pain will be worth it.";
     string message2 = "Blockchain, mark my words.";
     string message3 = unicode"I am fucking coming to get you. Asim Sheikh. 🚀";
 
